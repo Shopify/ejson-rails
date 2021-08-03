10c8090fe7a643b34ce96c446be9dd844540fb71
Masha Kuznetsova <masha.kuznetsova@shopify.com>
2021-08-03 09:45:55 -0400

    use double quotes as per rubocop rule

--- ejson-rails.gemspec
+++ ejson-rails.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^exe/}) { |f| File.basename(f) }
   spec.require_paths = ["lib"]
 
-  spec.required_ruby_version = '>= 2.6.0'
+  spec.required_ruby_version = ">= 2.6.0"
 
   spec.add_dependency("ejson")
   spec.add_dependency("railties", ">= 5.2")
--- spec/ejson/rails/railtie_spec.rb
+++ spec/ejson/rails/railtie_spec.rb
@@ -5,46 +5,46 @@ RSpec.describe(EJSON::Rails::Railtie) do
 
   let(:secrets) { secrets_class.new }
 
-  it 'should be a Railtie' do
+  it "should be a Railtie" do
     is_expected.to(be_a(::Rails::Railtie))
   end
 
-  context 'before configuration' do
+  context "before configuration" do
     before do
       allow_rails.to(receive(:root).and_return(fixtures_root))
-      allow_rails.to(receive_message_chain('application.secrets').and_return(secrets))
+      allow_rails.to(receive_message_chain("application.secrets").and_return(secrets))
     end
 
-    it 'merges secrets into application secrets' do
+    it "merges secrets into application secrets" do
       run_load_hooks
       expect(secrets).to(include(:secret))
     end
 
-    it 'prioritizes secrets.json' do
+    it "prioritizes secrets.json" do
       run_load_hooks
-      expect(secrets).to(include(secret: 'real_api_key'))
+      expect(secrets).to(include(secret: "real_api_key"))
     end
 
-    context 'without secrets.json' do
+    context "without secrets.json" do
       before { hide_secrets_files(secrets_json) }
 
-      it 'falls back to secrets.env.json' do
+      it "falls back to secrets.env.json" do
         expect(Rails).to(receive(:env).and_return(:env))
         run_load_hooks
-        expect(secrets).to(include(secret: 'test_api_key'))
+        expect(secrets).to(include(secret: "test_api_key"))
       end
 
-      it 'does not load anything when Rails.env doesn\'t match' do
+      it "does not load anything when Rails.env doesn\'t match" do
         expect(Rails).to(receive(:env).and_return(:production))
         run_load_hooks
         expect(secrets).to(be_empty)
       end
     end
 
-    context 'without any json' do
+    context "without any json" do
       before { hide_secrets_files(secrets_json, environment_secrets_json) }
 
-      it 'does not load anything' do
+      it "does not load anything" do
         expect(Rails).to(receive(:env).and_return(:production))
         run_load_hooks
         expect(secrets).to(be_empty)
